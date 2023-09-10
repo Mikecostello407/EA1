f2cf391faecd6596b0079e41404e7e5f47bcd2ba
Sythrex <ca.herreraa@duocuc.cl>
2023-09-10 00:55:55 -0300

    mike1.3

--- src/app/sugerencias/sugerencias.page.ts
+++ src/app/sugerencias/sugerencias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sugerencias',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SugerenciasPage implements OnInit {
 
-  constructor() { }
+  username: string = "";
+  password: string = "";
+
+  constructor(private router: Router) {} // Asegúrate de inyectar el servicio Router en el constructor
+
 
   ngOnInit() {
   }
+  login() {
+    if (this.username.trim() !== "" && this.password.trim() !== "") {
+      console.log('Usuario:', this.username);
+      console.log('Contraseña:', this.password);
 
+      this.username="";
+      this.password="";
+
+      this.router.navigate(['/home']);
+    } else {
+      console.log('Por favor, completa todos los campos.');
+    }
+  }
 }
